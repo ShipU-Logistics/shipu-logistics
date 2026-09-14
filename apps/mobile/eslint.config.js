8e9b56d88db1e2847ba810e2440a7030e3cdd6e3
ayush931 <ayushkumar9315983@gmail.com>
2026-09-14 11:56:59 +0530

    refactor: fixed the prettier issue

--- apps/mobile/eslint.config.js
+++ apps/mobile/eslint.config.js
@@ -2,8 +2,8 @@ import { defineConfig } from 'eslint/config';
 import expoConfig from 'eslint-config-expo/flat.js';
 
 export default defineConfig([
-  expoConfig,
-  {
-    ignores: ['dist/*'],
-  },
+    expoConfig,
+    {
+        ignores: ['dist/*'],
+    },
 ]);
